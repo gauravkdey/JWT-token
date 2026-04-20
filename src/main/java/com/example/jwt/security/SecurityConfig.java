0e3ada3a7b1ee0dc1e199daf9ab32f765f256505
gauravkdey <gauravkdey2005@gmail.com>
2026-04-20 14:53:51 +0530

    first commit

--- src/main/java/com/example/jwt/security/SecurityConfig.java
+++ src/main/java/com/example/jwt/security/SecurityConfig.java
@@ -1,16 +1,15 @@
 package com.example.jwt.security;
 
-import com.example.jwt.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -27,10 +26,10 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())   // 🔥 VERY IMPORTANT
             .authorizeHttpRequests(auth -> auth
-    .requestMatchers("/auth/**").permitAll()
-    .requestMatchers("/admin/**").hasRole("ADMIN")
-    .requestMatchers("/user/**").hasRole("USER")
-    .anyRequest().authenticated()
+    .requestMatchers("/", "/auth/**").permitAll()
+.requestMatchers("/admin/**").hasRole("ADMIN")
+.requestMatchers("/user/**").hasRole("USER")
+.anyRequest().authenticated()
 )
             .sessionManagement(sess -> sess
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
